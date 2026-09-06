300faa7273ddf825919b68289ae69ea593067107
Natureul <frogleggio@gmail.com>
2026-09-06 05:14:56 -0400

    Mark Bountiful board mixin pseudo for standalone compilation

--- runtime/src/main/java/com/natureul/cozycrazyquests/mixin/BoardBlockEntityMixin.java
+++ runtime/src/main/java/com/natureul/cozycrazyquests/mixin/BoardBlockEntityMixin.java
@@ -2,19 +2,21 @@ package com.natureul.cozycrazyquests.mixin;
 
 import com.natureul.cozycrazyquests.BountifulBridge;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(targets = "io.ejekta.bountiful.content.board.BoardBlockEntity", remap = false)
 public abstract class BoardBlockEntityMixin {
 
-    @Inject(method = "tryInitialPopulation", at = @At("HEAD"), remap = false)
+    @Inject(method = "tryInitialPopulation", at = @At("HEAD"), remap = false, require = 0)
     private void cozycrazyquests$stampRegionalDecree(CallbackInfo ci) {
         BountifulBridge.stampRegionalDecreeIfEmpty(this);
     }
 
-    @Inject(method = "randomlyUpdateBoard", at = @At("RETURN"), remap = false)
+    @Inject(method = "randomlyUpdateBoard", at = @At("RETURN"), remap = false, require = 0)
     private void cozycrazyquests$boundBoardPopulation(CallbackInfo ci) {
         BountifulBridge.maintainNoticeCount(this);
     }
