9017cf9fdc11c6fa11cf0f6b7e29c39e20be9dcf
Natureul <frogleggio@gmail.com>
2026-09-07 06:04:52 -0400

    Stop tracking Tunnel Gore kills

--- runtime/src/main/java/com/natureul/cozycrazyquests/CozyCrazyQuests.java
+++ runtime/src/main/java/com/natureul/cozycrazyquests/CozyCrazyQuests.java
@@ -34,7 +34,6 @@ public final class CozyCrazyQuests {
         MinecraftForge.EVENT_BUS.addListener(VillageConversationQuestManager::onPlayerTick);
         MinecraftForge.EVENT_BUS.addListener(GoreTunnelLead::onPlayerTick);
         MinecraftForge.EVENT_BUS.addListener(VillageConversationQuestManager::onLivingDeath);
-        MinecraftForge.EVENT_BUS.addListener(GoreTunnelLead::onLivingDeath);
         MinecraftForge.EVENT_BUS.addListener(VillageConversationQuestManager::onPlayerClone);
         MinecraftForge.EVENT_BUS.addListener(GoreTunnelLead::onPlayerClone);
         MinecraftForge.EVENT_BUS.addListener(VillageProgressState::onPlayerClone);
